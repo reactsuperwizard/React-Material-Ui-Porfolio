dce57541f7e712db0ef2d023b4fa5352b9429080
reactWizard012 <toprated0128@outlook.com>
2019-05-05 12:40:55 -0500

    feat: feed,add components

---
+++ src/components/Feed.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import Post from './Post';
+
+const Feed: React.FunctionComponent = () => {
+  return (
+    <>
+      <Box flex={4} p={2}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </Box>
+    </>
+  );
+};
+
+export default Feed;
